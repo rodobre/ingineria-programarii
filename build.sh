97dfd81a38f10fd7268f5c63b3c3085c82b45487
rodobre <dobre.florian.robert@gmail.com>
2021-02-19 13:49:10 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -g -Wall -I/include main.cpp -lpthread -o build/main
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "include/httplib/httplib.h"
+
+/**
+ * @brief Main driver function of the program, runs an HTTP server on port 8080 to process incoming requests (blocking!)
+ * 
+ * @return int - status code
+ */
+int main(void) {
+  httplib::Server svr;
+
+  svr.Get("/hi", [](const auto &, auto &res) {
+    res.set_content("Hello World!", "text/plain");
+  });
+
+  std::cout << "start server..." << std::endl;
+  svr.listen("0.0.0.0", 8080);
+}
